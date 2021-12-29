892220ac0528cc8acd2958ba326efed5fa29d78e
EliasRHansen <erh.roos@gmail.com>
2021-12-30 00:24:44 +0100

    added short description to methods

--- montecarlo.py
+++ montecarlo.py
@@ -15,6 +15,7 @@ import random as random
 class CBTmontecarlo:
     
     def __init__(self,N,offset_q,n0,U,kBT,Cs,offset_C,second_order_C):
+        
         self.N=N
         self.n0=n0
         self.Cs=Cs
@@ -23,11 +24,6 @@ class CBTmontecarlo:
         self.offset_q=offset_q
         self.offset_C=offset_C
         self.n0eff=self.neff(self.n0)
-        # self.n0eff=copy(self.n0)
-        # self.n0eff[0]=self.n0eff[0]-self.Cs[0]*self.U/2
-        # self.n0eff[1]=self.n0eff[1]-self.second_order_C[1]*self.U/2
-        # self.n0eff[-1]=self.n0eff[-1]+self.Cs[-1]*self.U/2
-        # self.n0eff[-2]=self.n0eff[-2]+self.second_order_C[-1]*self.U/2
         self.kBT=kBT
         d1=np.concatenate((self.Cs,np.zeros((1,))))
         dm1=np.concatenate((self.Cs,np.zeros((1,))))
@@ -50,6 +46,20 @@ class CBTmontecarlo:
         self.M=sparse.dia_matrix((dataM,offsetsM),shape=(self.N-1,self.N),dtype='float64').toarray()
     
     def neff(self,n):
+        """
+        
+
+        Parameters
+        ----------
+        n : charge array
+            DESCRIPTION.
+
+        Returns
+        -------
+        charge array adjusted for the fact that the potential at the boundaries are not otherwise included.
+            DESCRIPTION.
+
+        """
         self.neff=copy(n)
         self.neff[0]=self.neff[0]-self.Cs[0]*self.U/2
         self.neff[1]=self.neff[1]-self.second_order_C[1]*self.U/2
@@ -58,6 +68,25 @@ class CBTmontecarlo:
         return self.neff
     
     def energy(self,n):
+        """
+        
+
+        Parameters
+        ----------
+        n : charge array, or matrix of 2N charge arrays as columns
+            DESCRIPTION.
+
+        Raises
+        ------
+        Exception
+            If the charge array doesnt have the right shape.
+
+        Returns
+        -------
+        Total energy in the system, or array of total energies if the input is a matrix with charge arrays as columns
+            DESCRIPTION.
+
+        """
         if n.shape==(self.N-1,):
             v=self.Cinv@np.array([n]).T
             boundaries=(self.Cs[0]*v[0]+self.second_order_C[1]*v[1]-self.Cs[-1]*v[-1]-self.second_order_C[-1]*v[-2])*self.U/2
@@ -72,6 +101,20 @@ class CBTmontecarlo:
             raise Exception('energy could not be calculated due to incorrect shape of charge array')
 
     def iterable(self,m):
+        """
+        
+
+        Parameters
+        ----------
+        m : any type
+            DESCRIPTION.
+
+        Returns
+        -------
+        bool
+            true if the object passed is iterable and false otherwise.
+
+        """
         try:
             iter(m)
             return True
@@ -79,15 +122,59 @@ class CBTmontecarlo:
             return False
 
     def Q(self,n):
+        """
+        
+
+        Parameters
+        ----------
+        n : charge array
+
+        Returns
+        -------
+        Matrix with charge arrays as columns resulting from all possible transitions away from the provided charge configuration n
+            
+            DESCRIPTION.
+
+        """
         Qr=np.array([n]).T.repeat(self.N,axis=1)+self.M
         Ql=np.array([n]).T.repeat(self.N,axis=1)-self.M
         return np.concatenate((Qr,Ql),axis=1)
     def Q0(self,n):
+        """
+        
+
+        Parameters
+        ----------
+        n : charge array
+            DESCRIPTION.
+
+        Returns
+        -------
+        Matrix with copies of current charge array as columns
+        
+            DESCRIPTION.
+
+        """
         Qr=np.array([n]).T.repeat(self.N,axis=1)
         return np.concatenate((Qr,Qr),axis=1)
 
     def transition_rate(self,n2,n1):
+        """
         
+
+        Parameters
+        ----------
+        n2 : charge array or matrix of charge arrays as columns
+            DESCRIPTION.
+        n1 : charge array or matrix of charge arrays as columns
+            DESCRIPTION.
+
+        Returns
+        -------
+        Gamma : array of transition rates calculated from the energy differences between charge configuration n2 and n1
+            DESCRIPTION.
+
+        """
         dE=self.energy(n2)-self.energy(n1)
         limit1=1e-15
         limit2=1e15
@@ -108,13 +195,41 @@ class CBTmontecarlo:
             return Gamma
 
     def P(self,n):
+        """
         
+
+        Parameters
+        ----------
+        n : current charge configuration
+            DESCRIPTION.
+
+        Returns
+        -------
+        array of 2N transition probabilities where the first N represents a transition from Right to left and the second N represents transitions from left to right.
+            DESCRIPTION.
+
+        """
         p=self.transition_rate(self.Q(n),self.Q0(n))
         
         return p/sum(p)
 
     def pick_event(self,n,k):
+        """
         
+
+        Parameters
+        ----------
+        n : current charge configuration
+            DESCRIPTION.
+        k : number of events
+            DESCRIPTION.
+
+        Returns
+        -------
+        index : indices of events chosen
+            DESCRIPTION.
+
+        """
         index=random.choices(np.arange(2*self.N),weights=self.P(n),k=k)
         
         return index
