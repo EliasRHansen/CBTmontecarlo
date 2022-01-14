found a factor 2 mistake in the energy calculation. It only seems to affect the result at very high temperatures, but I probably have to redo the runs:/

electronsjumparound.py
@@ -351,6 +351,7 @@ class CBTmain: #just does the simulation, no further analysis
             self.gi=np.tile(self.Cs,int(len(bound)/self.N))
             # self.gi2=self.gi.reshape(self.number_of_concurrent*self.number_of_Us,2*self.N)
             print('gi:'+str(self.gi))
+            # print(bound/C)
             self.dE0=C+bound
         # else:
         #     print(str(self.parallelization)+' is not implemented as a value for the parameter parallelization')
@@ -377,7 +378,9 @@ class CBTmain: #just does the simulation, no further analysis
         E[self.N-1::2*self.N]=E[self.N-1::2*self.N]+self.boundary_works
         E[self.N::2*self.N]=E[self.N::2*self.N]-self.boundary_works
         E[2*self.N-1::2*self.N]=E[2*self.N-1::2*self.N]-self.boundary_works
-
+        # print('0:'+str(self.dE0[0]/E[0]))
+        # print(self.dE0[34]/E[34])
+        # print(np.mean(self.dE0/E))
         return E
     def update_transition_rate(self,n1):
         """
@@ -700,9 +703,9 @@ class CBT_data_analysis:
                 number_of_time_steps_with_data=len(CBT.dQp)
                 dQ=np.empty((len(Us),number_of_time_steps_with_data,nc))
                 dt=np.empty((len(Us),number_of_time_steps_with_data,nc))
-                for j in np.arange(len(CBT.dQp)):
-                    i1=CBT.dQp[j]
-                    i2=CBT.dtp[j]
+                for j in np.arange(len(CBT.dQps)):
+                    i1=CBT.dQps[j]
+                    i2=CBT.dtps[j]
                     dQ1=np.array(i1).reshape(number_of_time_steps_with_data,batchlengths[j],nc)
                     dt1=np.array(i2).reshape(number_of_time_steps_with_data,batchlengths[j],nc)
                     dQ1=np.array(dQ1).swapaxes(1,0)

examples.py
@@ -17,16 +17,16 @@ e_SI=1.602*1e-19
 N=100 #Number of islands
 Ec=4.6e-6 #Charging energy in units of eV
 Gt=2.16e-5 #Large voltage asymptotic conductance (affects noly the scaling of the result)
-T=0.020 #Temperature in Kelvin
+T=0.050 #Temperature in Kelvin
 FWHM=5.439*kB*T*N #Full width half max according to the first order model
-points=101 #number of voltages to run the simulation for
-lim=1.5*FWHM
+points=11 #number of voltages to run the simulation for
+lim=3*FWHM
 V=np.linspace(-lim,lim,points)
 
 
 ####Run main simulation####
-res=carlo_CBT(V,T,Ec,Gt,N=N,Nruns=10000,Ninterval=1000,Ntransient=10000,n_jobs=2,number_of_concurrent=8,
-              parallelization='external',q0=0,dV=FWHM/50,batchsize=10)
+res=carlo_CBT(V,T,Ec,Gt,N=N,Nruns=10000,Ninterval=2000,Ntransient=10000,n_jobs=2,number_of_concurrent=8,
+              parallelization='internal',q0=0,dV=FWHM/50,batchsize=10,transient =1)
 
 
 ####store main results###
@@ -185,8 +185,8 @@ idd_1=1859
 
 dGs,voltages,currents,dGs_std,voltages_std,currents_std,dGs_av,voltages_av,currents_av,idds=load_data(idd_0,idd_1)
 
-V_data=voltages_av[400:1100][::10]
-G_data=dGs_av[400:1100][::10]
+V_data=voltages_av[400:1100]
+G_data=dGs_av[400:1100]
 plt.figure()
 plt.plot(V_data,G_data,'.')
 p0=[30e-3,4e-6,2.17e-5,0]
@@ -482,17 +482,18 @@ for u in unitless_u:
         plt.close()
 #%%
 #store data
-us=np.linspace(0.1,4,40)+0.025
-lim=5.2*5.439*N
-points=131
+us=4e-6/(kB*np.linspace(5e-3,16e-3,12))
+# us=np.linspace(0.1,4,40)+0.025
+lim=4.5*5.439*N
+points=201
 V=np.linspace(-lim,lim,points)
 number_of_concurrent=20
-q0=0
+q0=np.random.uniform(low=-1,high=1,size=(N-1,))
 n_jobs=2
-Nruns=30000
-Ninterval=10
-Ntransient=330000
-transient=500
+Nruns=55000
+Ninterval=4
+Ntransient=300000
+transient=1500
 for u in us:
     print(u)
     res=carlo_CBT(V,1/kB,u,1,N=N,Nruns=Nruns,Ninterval=Ninterval,Ntransient=Ntransient,n_jobs=2,number_of_concurrent=number_of_concurrent,
@@ -502,7 +503,27 @@ for u in us:
         plt.pause(0.05)
         plt.show()
     res.savedata()
+#%%
+path_to_data=os.getcwd()+'\\Final data 2\\'
+all_files = list()
+dirname=list()
+dirpaths=list()
+models=list()
+pars=list()
+for (dirpath, dirnames, filenames) in os.walk(path_to_data):
+    print(1)
+    npz_files = list(filter(lambda x: x[-4:] == '.npz', filenames))
+    print(npz_files)
+    if len(npz_files) != 0:
+        all_files += npz_files
+        dirname+=dirnames
+        dirpaths+=[dirpath+'\\']
 
+        fit_result=fit_carlo(V_data,G_data, filename=os.path.join(dirpath,npz_files[0]),plot=True,save=False,save_fig_folder=dirpath+'\\')
+        if (np.mean(fit_result.offset_C)==0) and (np.mean(fit_result.second_order_C)==0) and (np.mean(fit_result.q0)==0):
+            
+            models.append((fit_result.model,fit_result.u))
+            pars.append(fit_result.par)
 #%%
 #folder_with_results='C:\\Users\\Elias Roos Hansen\\Documents\\Københavns uni\\qdev\\code\Runs with too much data'+'\\Results 2022-01-10 20.56.02.630781, sim time=1672.6sec\\'
 path_to_data='C:\\Users\\Elias Roos Hansen\\Documents\\Københavns uni\\qdev\\code\Runs with too much data'